6894c3ee19d8777f721238b3673634b7ac54918b
ragul0427 <ragulhp2704@gmail.com>
2023-10-05 15:02:12 +0530

    e;rreb

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const cookieParser = require('cookie-parser');
 app.use(cookieParser());
 app.use(express.json())
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: process.env.ORIGIN,
     credentials: true,
 }));
 
